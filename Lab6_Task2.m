738d135be6ead0cb03f027aed0a84d8bb5bc0ff8
crustaceandrip <alexanderbechta17@gmail.com>
2025-05-06 09:25:38 -0400

    gwop

--- Lab6_Task2.m
+++ Lab6_Task2.m
@@ -71,12 +71,13 @@ title('Mid Range 2')
 %% Task 4
 
 s_h = 1j*f_h*2*pi;
-s_l = 1j*f_l;
-s_m2 = 1j*f_m2;
+s_l = 1j*f_l*2*pi;
+s_m2 = 1j*f_m2*2*pi;
 
 R = 80e3;
 C = 10e-9;
 
+% High Range
 Rp_guess = 10000;
 Rp_h = fminsearch(@(Rp) Error_Mag(s_h,Rp,mag_h,C,R),Rp_guess);
 
@@ -95,6 +96,7 @@ ylabel('Magnitude (dB)')
 
 
 
+% Low Range
 Rp_l = fminsearch(@(Rp) Error_Mag(s_l,Rp,mag_l,C,R),Rp_guess);
 
 Giw = -C*R*(Rp_l+100)*s_l./( (C^2)*(R^2)*(Rp_l+100)*s_l.^2 + C*R*(Rp_l+100)*s_l +(R+Rp_l+100)/2 );
@@ -111,7 +113,8 @@ xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
 
 
-Rp_guess = 10000;
+% Mid Range 2
+Rp_guess = 8000;
 Rp_m2 = fminsearch(@(Rp) Error_Mag(s_m2,Rp,mag_m2,C,R),Rp_guess);
 
 Giw = -C*R*(Rp_m2+100)*s_m2./( (C^2)*(R^2)*(Rp_m2+100)*s_m2.^2 + C*R*(Rp_m2+100)*s_m2 +(R+Rp_m2+100)/2 );
