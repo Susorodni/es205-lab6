1412372cc5e3f7532284ede3d95fd9d8c2cea7e9
Luke Gaston <lgaston@myhoosierneighbors.com>
2025-05-06 09:28:33 -0400

    Created seperate plots for phase and magnitude for Task 1

--- Lab6_Task1.m
+++ Lab6_Task1.m
@@ -11,6 +11,7 @@ s = 1j*w;
 R = 80e3;
 C = 10e-9;
 
+%% Plot 1 - Magnitude Plot
 for Rp = 0:1000:10000
 
     Giw_1 = -C*R*(Rp+100)*s./( (C^2)*(R^2)*(Rp+100)*s.^2 + C*R*(Rp+100)*s +(R+Rp+100)/2 );        %  FRF – use ‘.’ operators
@@ -20,20 +21,33 @@ for Rp = 0:1000:10000
                                            %  around the unit circle.
     
     
-    set(gcf,'Position',[50 50 1300 750])
-    set(gcf, 'color', 'w')
-    subplot(211)             %  Create a subplot figure
+    % set(gcf,'Position',[50 50 1300 750])
+    % set(gcf, 'color', 'w')
+    % subplot(211)             %  Create a subplot figure
     plot(w/(2*pi),MdB_1)          %  Bode magnitude plot
     hold on
     grid on
     xlabel('Frequency (Hz)')
     ylabel('Magnitude (dB)')
+end
 
-    subplot(212)
+%% Plot 2 - Phase Plot
+figure
+for Rp = 0:1000:10000
+
+    Giw_1 = -C*R*(Rp+100)*s./( (C^2)*(R^2)*(Rp+100)*s.^2 + C*R*(Rp+100)*s +(R+Rp+100)/2 );        %  FRF – use ‘.’ operators
+    magGiw_1 = abs(Giw_1);       %  FRF magnitude
+    MdB_1 = 20*log10(magGiw_1);  %  Magnitude in dB
+    phaseGiw_1 = unwrap(angle(Giw_1))*180/pi;  %  FRF phase in degrees.  Unwrap prevents jumps
+                                           %  around the unit circle.
+    
+    
+    % set(gcf,'Position',[50 50 1300 750])
+    % set(gcf, 'color', 'w')
+    % subplot(211)             %  Create a subplot figure
     plot(w/(2*pi),phaseGiw_1)     %  Bode phase plot
     hold on
     grid on
     xlabel('Frequency (Hz)')
     ylabel('Phase (deg)')	
-
 end
